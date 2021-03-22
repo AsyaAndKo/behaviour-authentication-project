6b23ec374ebc98fa45cd74859cddf6a6629d100b
AsyaAndKo <asya.kosareva666@gmail.com>
2021-03-22 21:08:23 +0200

    Create small Android App
    Add button click counter
    Add screen tap counter
    Add timer for each tap
    Add login and password text inputs also save them to string

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [countButton, setCountButton] = useState(0);
+  const [countScreen, setCountScreen] = useState(0);
+  const [swipeTimer, setSwipeTimer] = useState(0);
+
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [text, setText] = useState("");
+
+  let start, end;
+
+  const styles = StyleSheet.create({
+    default: {
+      padding: 30,
+      width: "100%",
+      height: "100%",
+      backgroundColor: "transparent",
+      flex: 1,
+      justifyContent: "center",
+    },
+    entrySpace: {
+      borderWidth: 1,
+      borderStyle: "solid",
+      borderColor: "#000",
+      fontSize: 15,
+      padding: 5,
+      marginTop: 5,
+      marginBottom: 5,
+    },
+    loginForm: {
+      textAlign: "center",
+      fontSize: 25,
+    },
+  });
+
+  let logPlusPass = [];
+  const signIn = () => {
+    if (login == undefined || password == undefined) {
+      setText(" ");
+    } else {
+      logPlusPass.push(login);
+      logPlusPass.push(password);
+      setText(`Email: ${logPlusPass[0]} \nPassword: ${logPlusPass[1]}`);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={styles.default}
+      onTouchStart={() => {
+        start = new Date().getTime();
+      }}
+      onTouchEnd={() => {
+        setCountScreen(countScreen + 1);
+        end = new Date().getTime();
+        setSwipeTimer(end - start);
+      }}
+    >
+      <Text style={styles.loginForm}>Login form</Text>
+      <TextInput
+        style={styles.entrySpace}
+        placeholder="Login"
+        onChangeText={(login) => setLogin(login)}
+      />
+      <TextInput
+        style={styles.entrySpace}
+        placeholder="Password"
+        onChangeText={(password) => setPassword(password)}
+      />
+      <Button title="Sign in" onPress={signIn} />
+      <Text>{text}</Text>
+
+      <Text>{`\nswipe time: ${swipeTimer}`}</Text>
+
+      <Text>click on screen: {countScreen}</Text>
+
+      <Text>click on Button: {countButton}</Text>
+
+      {/* TODO: Change button to touchable opacity, so I'll be able to style it properly */}
+      <Button
+        color="#dea4eb"
+        title="Change Text"
+        onPress={() => setCountButton(countButton + 1)}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
